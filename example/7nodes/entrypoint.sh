4cee8316b96fad3fcf93a4473c02ce5557c5024e
Fabio Bigmoby Mauro <fabio.mauro@databiz.it>
2018-02-06 22:24:04 +0100

    fixing shell script

--- example/7nodes/entrypoint.sh
+++ example/7nodes/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sh ./home/workspace/example/7nodes/raft-init.sh
-
-sh ./home/workspace/example/7nodes/raft-start.sh
+/bin/bash -c "source /home/workspace/example/7nodes/raft-init.sh"
+sleep 10
+/bin/bash -c "source /home/workspace/example/7nodes/raft-start.sh"
